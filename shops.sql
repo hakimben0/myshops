6de72019272072015ed559b0ff2f9abcf48e5d25
hakimben0 <34188248+hakimben0@users.noreply.github.com>
2017-12-20 00:10:52 +0000

    Database dump

--- shops.sql
+++ shops.sql
@@ -1,26 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 4.7.4
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1:3306
--- Generation Time: Dec 20, 2017 at 12:06 AM
--- Server version: 5.7.19
--- PHP Version: 5.6.31
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `shops`
---
 CREATE DATABASE IF NOT EXISTS `shops` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `shops`;
 
